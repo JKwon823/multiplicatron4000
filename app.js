d584630691980a561d52b7206da7fe108399c9f5
Joseph Kwon <kwonjosepho@gmail.com>
2017-04-15 03:58:46 -0700

    connected event handlers multiplicatron4000

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 const mult = require('./multiplier.js');
 
-console.log(mult(2, 4));
+class ViewManager {
+
+	connectEventHandlers() {
+		document.getElementById('form-numbers')
+			.addEventListener('submit', this.onSubmit);
+	}
+
+	onSubmit(event) {
+
+		event.preventDefault();
+
+		let num1 = document.getElementById('input-num1').value;
+		let num2 = document.getElementById('input-num2').value;
+
+		num1 = parseInt(num1, 10);
+		num2 = parseInt(num2, 10);
+
+		const product = mult(num1, num2);
+
+		alert(product);
+	}
+}
+
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();
