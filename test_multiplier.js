c7963259e43f70495e7464e291f16ea06939c830
Joseph Kwon <kwonjosepho@gmail.com>
2017-04-15 03:20:13 -0700

    initial commit for multiplicatron4000

---
+++ test_multiplier.js
@@ -0,0 +1,37 @@
+const mult = require('./multiplier.js');
+
+class TestSuite {
+	
+	runTest(testName) {
+		const result = this[testName]();
+		console.log(result, testName);
+	}
+
+	runTests(){
+		//find tests
+		//execute them
+		Object.getOwnPropertyNames(Object.getPrototypeOf(this))
+			.filter(prop => this[prop] instanceof Function)
+			.filter(prop => prop.indexOf('test') !== -1)
+			.forEach(testName => this.runTest(testName));
+	}
+
+	assertEquals(a, b) {
+		return a === b;
+	}
+
+	testMultPositiveNumbers(){
+		return this.assertEquals(mult(3, 2), 6);
+	}
+
+	testMultPositiveAndNegativeNumbers(){
+		return this.assertEquals(mult(-3, 4), -12);
+	}
+
+	testMultNegativeNumbers(){
+		return this.assertEquals(mult(-3, -1), 3);
+	}	
+}
+
+const testSuite = new TestSuite();
+testSuite.runTests();
