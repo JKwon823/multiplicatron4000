4121bb471f05e7c6c907154b484177ab6030d75a
Joseph Kwon <kwonjosepho@gmail.com>
2017-04-18 08:12:20 -0700

    fixed some spacing on multiplicatron4000

--- client/multiplier.js
+++ client/multiplier.js
@@ -1,15 +1,15 @@
 const mult = function(numbers) {
 	let answer = NaN;
 	numbers = numbers.filter(input => typeof input === 'number');
-		if (numbers.length > 1) {
-			numbers.forEach((number, index) => {
-				if (index === 0) {
-					answer = number;
-				} 	else {
-						answer *= number;
-					}
-			});
-		}
+	if (numbers.length > 1) {
+		numbers.forEach((number, index) => {
+			if (index === 0) {
+				answer = number;
+			} 	else {
+					answer *= number;
+				}
+		});
+	}
 	return answer;
 };
 
